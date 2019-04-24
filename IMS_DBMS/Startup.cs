90f1417ef9886427505267808efaa47682bbecbd
2016CS112 <38519481+2016CS112@users.noreply.github.com>
2019-04-24 15:42:01 +0500

    Login SignUp CourseView Done

---
+++ IMS_DBMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IMS_DBMS.Startup))]
+namespace IMS_DBMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
